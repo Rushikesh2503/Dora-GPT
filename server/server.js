50014d31c4f47c8795c64957ecbd4c7985ba7c09
RushikeshB7 <rushikesh@salesken.ai>
2023-02-25 01:15:32 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -15,6 +15,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
